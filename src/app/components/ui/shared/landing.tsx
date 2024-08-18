78a98bef95c55e6e120d40990a0d032f389a79c8
Nyirongo2000 <bed-com-21-19@unima.ac.mw>
2024-08-18 22:44:05 +0200

    image align

--- src/app/components/ui/shared/landing.tsx
+++ src/app/components/ui/shared/landing.tsx
@@ -1,39 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
-export default function Landing(){
+
+export default function Landing() {
   return (
-    <main className="flex min-h-screen flex-row  justify-between p-24">
+    <main className="flex min-h-screen flex-row  p-10">
       {/* Flexbox Container for Intro Text and Numbers */}
-      <div className="flex flex-col w-full justify-between">
-        <div className="flex">
-          <p>
-            Where Design Meets Development: <br/>
-            Explore My Creative & Technical Masterpieces
-          </p>
-        </div>
-        <div className="flex flex-col w-full justify-between ">
-          <div className="flex text-center">
-            <p>code Portfolio</p>
+      <div className="flex flex-row justify-between ">
+        <div className="flex flex-col">
+          <div className="flex">
+            <p>
+              Where Design Meets Development: <br />
+              Explore My Creative & Technical Masterpieces
+            </p>
           </div>
-          <div className="flex text-right">
-            <p>design Portfolio</p>
+          <div className="flex flex-col  mt-4">
+            <div>
+              <p>code Portfolio</p>
+            </div>
+            <div>
+              <p>design Portfolio</p>
+            </div>
+          </div>
+          <div className="mt-4">
+            <p>get in touch</p>
           </div>
         </div>
-        <div className="flex">
-          <p>get in touch</p>
+
+        <div className="mt-[-10rem] mr-[-17rem]">
+          <Image
+            src="/resources/self.png"
+            alt="Description of image"
+            width={700}
+            height={1}
+            className="object-cover"
+          />
         </div>
       </div>
 
       {/* Image Section */}
-      <div className="flex justify-between w-full">
-        <Image
-          src="/path-to-your-image.jpg" // Replace with the actual path to your image
-          alt="Description of image"
-          width={500} // Set your desired width
-          height={300} // Set your desired height
-          className="object-cover"
-        />
-      </div>
     </main>
   );
 };
