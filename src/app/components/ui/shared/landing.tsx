5542f02b5fa4061c156619f2d0bdd83f1c41d58a
Nyirongo2000 <bed-com-21-19@unima.ac.mw>
2024-08-19 15:45:48 +0200

    done with landing page

--- src/app/components/ui/shared/landing.tsx
+++ src/app/components/ui/shared/landing.tsx
@@ -1,9 +1,10 @@
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Landing() {
   return (
-    <main className="flex min-h-screen flex-row  p-10">
+    <main className="flex min-h-screen flex-row justify-between p-10">
       {/* Flexbox Container for Intro Text and Numbers */}
       <div className="flex flex-row justify-between ">
         <div className="flex flex-col">
@@ -13,16 +14,33 @@ export default function Landing() {
               Explore My Creative & Technical Masterpieces
             </p>
           </div>
-          <div className="flex flex-col  mt-4">
+          <div className="flex flex-row justify-between  mt-4">
             <div>
-              <p>code Portfolio</p>
+              <Link
+                href="/pages/CodePortfolio"
+                className="flex items-center gap-5 self-start rounded-lg bg-slate-900 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-slate-700 md:text-base "
+              >
+                <span>code Portfolio</span>{" "}
+                {/* <ArrowRightIcon className="w-5 md:w-6" /> */}
+              </Link>
             </div>
             <div>
-              <p>design Portfolio</p>
+              <Link
+                href="/pages/DesignPortfolio"
+                className="flex items-center gap-5 self-start rounded-lg bg-slate-900 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-slate-700 md:text-base"
+              >
+                <span>Design Portfolio</span>{" "}
+            
+              </Link>
             </div>
           </div>
           <div className="mt-4">
-            <p>get in touch</p>
+            <Link
+              href="/pages/CodePortfolio"
+              className="flex items-center justify-center text-center rounded-lg bg-orange-700 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-orange-900 md:text-base"
+            >
+              <p className="m-0">get in touch</p>
+            </Link>
           </div>
         </div>
 
@@ -40,4 +58,4 @@ export default function Landing() {
       {/* Image Section */}
     </main>
   );
-};
+}
